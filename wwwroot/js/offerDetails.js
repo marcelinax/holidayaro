db810f33831fea8dfaa3f671cbf57c4362fb0a68
Marcelina Sitko <marcelina.sitko@interia.pl>
2021-10-22 19:54:19 +0200

    image modal + change current image

--- wwwroot/js/offerDetails.js
+++ wwwroot/js/offerDetails.js
@@ -3,15 +3,32 @@ const proposedOffersBox = document.querySelector('.current-offer-bottom-offers-b
 const hotelMenuLink = document.getElementById('hotel-btn');
 const roomMenuLink = document.getElementById('room-btn');
 const boardMenuLink = document.getElementById('board-btn');
-const currentOfferImg = document.querySelector('.current-offer-bg')
+const currentOfferImg = document.querySelector('.current-offer-bg');
+const galleryImages = document.querySelectorAll('.current-offer-top-box-gallery-item');
+const imageModalImg = document.querySelector('.image-modal-img');
+const imageModalBox = document.querySelector('.image-modal');
+const closeImageModalBtn = document.getElementById('close-image-modal');
 
 let currentMenuLink = 'hotel';
 let hotels = [];
 
 
 const setCurrentOfferImg = (img) => {
-    console.log(img)
-    currentOfferImg.style.backgroundImage = img ;
+    galleryImages.forEach(galleryImg => {
+        galleryImg.classList.remove('current');
+        if (galleryImg.firstElementChild.style.backgroundImage.substring(5, galleryImg.firstElementChild.style.backgroundImage.length - 2) == img)
+        {
+            galleryImg.classList.add('current');
+        }
+        });
+   
+
+    currentOfferImg.style.backgroundImage = `url(${img})`;
+}
+
+const showImgModal = (img) => {
+    imageModalImg.style.backgroundImage = `url(${img})`;
+    imageModalBox.classList.add('show');
 }
 
 
@@ -20,7 +37,7 @@ const renderStars = (rating) => {
     const starsCount = Math.ceil(rating / 2);
     for (let i = 1; i <= starsCount; i++) {
         stars += `<i class='bx bxs-star' ></i>`;
-     
+
     };
     for (let i = starsCount; i < 5; i++) {
         stars += `<i class='bx bx-star' ></i>`;
@@ -36,10 +53,10 @@ const renderProposedOffers = () => {
         randomHotels.push(hotels[randomHotel]);
     }
     console.log(randomHotels)
-        randomHotels.forEach(hotel => {
-            const proposedOfferBox = document.createElement('div'); 
-            proposedOfferBox.classList.add('current-offer-bottom-offers-box-item');
-            const content = `
+    randomHotels.forEach(hotel => {
+        const proposedOfferBox = document.createElement('div');
+        proposedOfferBox.classList.add('current-offer-bottom-offers-box-item');
+        const content = `
         <div class='col-6 current-offer-bottom-offers-box-item-img px-2' style="background-image: url(${hotel.photosUrls.$values[0].photoUrl})"></div>
         <div class='col-6 current-offer-bottom-offers-box-item-info px-2'>
          <p class='name'>${hotel.name}</p>
@@ -48,15 +65,15 @@ const renderProposedOffers = () => {
         <div class='stars-box'></div>
         </div>
     `;
-            proposedOfferBox.innerHTML = content;
-            proposedOfferBox.addEventListener('click', () => {
-                location.pathname = `hotels/details/${hotel.hotelId}`;
-            })
-            const proposedOfferStarBox = proposedOfferBox.querySelector('.stars-box');
-            proposedOfferStarBox.innerHTML = renderStars(hotel.rating);
-            proposedOffersBox.appendChild(proposedOfferBox);
+        proposedOfferBox.innerHTML = content;
+        proposedOfferBox.addEventListener('click', () => {
+            location.pathname = `hotels/details/${hotel.hotelId}`;
         })
-   
+        const proposedOfferStarBox = proposedOfferBox.querySelector('.stars-box');
+        proposedOfferStarBox.innerHTML = renderStars(hotel.rating);
+        proposedOffersBox.appendChild(proposedOfferBox);
+    })
+
 
 }
 
@@ -79,7 +96,7 @@ const renderInformationAboutHotel = () => {
         hotelMenuLink.classList.add('active');
         roomMenuLink.classList.remove('active');
         boardMenuLink.classList.remove('active');
-       
+
     }
     if (currentMenuLink === 'room') {
         roomInformationBox.classList.remove('d-none');
@@ -102,20 +119,23 @@ const renderInformationAboutHotel = () => {
 starsBox.innerHTML = renderStars(hotelRating);
 hotelMenuLink.addEventListener('click', () => {
     currentMenuLink = 'hotel'
-   
+
     renderInformationAboutHotel();
- 
+
 });
 roomMenuLink.addEventListener('click', () => {
     currentMenuLink = 'room';
-   
+
     renderInformationAboutHotel();
 });
-boardMenuLink.addEventListener('click', () =>  {
+boardMenuLink.addEventListener('click', () => {
     currentMenuLink = 'board';
-   
+
     renderInformationAboutHotel();
 });
 
 renderInformationAboutHotel();
 getAllHotels();
+galleryImages[0].classList.add('current');
+currentOfferImg.addEventListener('click', () => showImgModal(currentOfferImg.style.backgroundImage.substring(5, currentOfferImg.style.backgroundImage.length - 2)));
+closeImageModalBtn.addEventListener('click', () => imageModalBox.classList.remove('show'))
